ec5e1e5aaa3811525b3afede8a9be529c7b617bf
martinmaeland <martinmaeland@outlook.com>
2021-04-16 22:46:36 +0200

    added dependencies install script

---
+++ tools/install_dependencies_ubuntu.sh
@@ -0,0 +1,6 @@
+echo "Installing dependencies for Robot_Path_Generator..."
+
+python3 -m pip install pygame==2.0.1
+
+echo
+echo "All dependencies are up to date"
---
+++ tools/install_dependencies_windows.bat
@@ -0,0 +1,6 @@
+ECHO "Installing dependencies for Robot_Simulator..."
+
+py -m pip install pygame==2.0.1
+
+ECHO
+ECHO "All dependencies are up to date"
